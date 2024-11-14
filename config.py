cf8403066e0565e99cee78fe5b66eb2ae24f0dd7
MAULANAadib <123590047+MAULANAadib@users.noreply.github.com>
2024-11-15 02:17:02 +0700

    config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 #Configuring MySQL database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://PemwebUTS_writtenits:5c37e90d0620616381e1cf3e0e5aa5ff79b7e2ee@dco2x.h.filess.io:3307/PemwebUTS_writtenits'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://UTSPemweb_notwouldby:a775ce8801b476528676d517b55c48a87407831c@nahkj.h.filess.io:3306/UTSPemweb_notwouldby'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
